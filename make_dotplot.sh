1db6450fa17c005bc6fe16c3686ef4fd3108c90d
Charles Plessy <charles.plessy@oist.jp>
2020-07-06 15:08:08 +0900

    Update to generic names.

--- make_dotplot.sh
+++ make_dotplot.sh
@@ -5,47 +5,43 @@ module load LAST
 module load samtools
 module load blast
 module load emboss
-infoseq @g1_V14.usa -length -name -only -nohead -filter > g1.lengths.txt
-python bed_file_generator.py G1 g1.lengths.txt > oskaV14.bed
-infoseq @g2_V14.usa -length -name -only -nohead -filter > g2.lengths.txt
-python bed_file_generator.py G2 g2.lengths.txt >> oskaV14.bed
-infoseq @g3_V14.usa -length -name -only -nohead -filter > g3.lengths.txt
-python bed_file_generator.py G3 g3.lengths.txt >> oskaV14.bed
-infoseq @g4_V14.usa -length -name -only -nohead -filter > g4.lengths.txt
-python bed_file_generator.py G4 g4.lengths.txt >> oskaV14.bed
-infoseq @g5_V14.usa -length -name -only -nohead -filter > g5.lengths.txt
-python bed_file_generator.py G5 g5.lengths.txt >> oskaV14.bed
-infoseq @g6_V14.usa -length -name -only -nohead -filter > g6.lengths.txt
-python bed_file_generator.py G6 g6.lengths.txt >> oskaV14.bed
-infoseq @g7_V14.usa -length -name -only -nohead -filter > g7.lengths.txt
-python bed_file_generator.py G7 g7.lengths.txt >> oskaV14.bed
-infoseq @g8_V14.usa -length -name -only -nohead -filter > g8.lengths.txt
-python bed_file_generator.py G8 g8.lengths.txt >> oskaV14.bed
-union @g1_V14.usa g1_V14.fa
-union @g2_V14.usa g2_V14.fa
-union @g3_V14.usa g3_V14.fa
-union @g4_V14.usa g4_V14.fa
-union @g5_V14.usa g5_V14.fa
-union @g6_V14.usa g6_V14.fa
-union @g7_V14.usa g7_V14.fa
-union @g8_V14.usa g8_V14.fa
-sed -i "1s/.*/>G1/" g1_V14.fa
-sed -i "1s/.*/>G2/" g2_V14.fa
-sed -i "1s/.*/>G3/" g3_V14.fa
-sed -i "1s/.*/>G4/" g4_V14.fa
-sed -i "1s/.*/>G5/" g5_V14.fa
-sed -i "1s/.*/>G6/" g6_V14.fa
-sed -i "1s/.*/>G7/" g7_V14.fa
-sed -i "1s/.*/>G8/" g8_V14.fa
-seqret @oskaV14.usa oskaV14.fa
-mkdir /work/LuscombeU/Oikopleura/Genomes/OskaV14
-cp /work/LuscombeU/Jan/oskaV14.fa /work/LuscombeU/Oikopleura/Genomes/OskaV14/OskaV14.fa
-srun indexGenome.py -g OskaV14
-sbatch mkDotPlot.py -x OskaV14 -y O9-3f  --dotplot-options='--bed1=/work/LuscombeU/Jan/oskaV14.bed'
-sbatch mkDotPlot.py -x OskaV14 -y O3-1f  --dotplot-options='--bed1=/work/LuscombeU/Jan/oskaV14.bed'
-sbatch mkDotPlot.py -x OskaV14 -y I69-4  --dotplot-options='--bed1=/work/LuscombeU/Jan/oskaV14.bed'
-sbatch mkDotPlot.py -x OskaV14 -y Norway  --dotplot-options='--bed1=/work/LuscombeU/Jan/oskaV14.bed'
-sbatch mkDotPlot.py -x OskaV14 -y AOM-5-1f  --dotplot-options='--bed1=/work/LuscombeU/Jan/oskaV14.bed'
-sbatch mkDotPlot.py -x OskaV14 -y OSK42-1f  --dotplot-options='--bed1=/work/LuscombeU/Jan/oskaV14.bed'
+
+mkLenghts () {
+  infoseq @$1.usa -length -name -only -nohead -filter > $1.lengths.txt
+  python bed_file_generator.py $1 $1.lengths.txt >> oskaV2.bed
+}
+
+mkFASTA () {
+  union @$1.usa $1.fa
+  sed -i "1s/.*/>$1/" $1.fa
+}
+
+rm oskaV2.bed
+
+for chr in Chr1 Chr2 PAR XSR YSR
+do
+  mkLenghts $chr
+  mkFASTA   $chr
+done
+
+VERSION=V15
+
+mkdir /work/LuscombeU/Oikopleura/Genomes/Oska$VERSION
+
+srun seqret @oskaV2.usa /work/LuscombeU/Oikopleura/Genomes/Oska$VERSION/oska$VERSION.fa
+srun indexGenome.py -g Oska$VERSION
+
+alignTo () {
+  sbatch mkDotPlot.py -x Oska$VERSION -y $1 \
+    --dotplot-options='--bed1=~/OSKA_superscaffolding/oskaV2.bed'
+}
+
+for genome in I69-4 Norway OSK42-1f AOM-5-1f O9-5f O3-3f B-2-1f
+
+sbatch mkDotPlot.py -x Oska$VERSION -y O3-1f  --dotplot-options='--bed1=~/OSKA_superscaffolding/oskaV2.bed'
+sbatch mkDotPlot.py -x Oska$VERSION -y I69-4  --dotplot-options='--bed1=~/OSKA_superscaffolding/oskaV2.bed'
+sbatch mkDotPlot.py -x Oska$VERSION -y Norway  --dotplot-options='--bed1=~/OSKA_superscaffolding/oskaV2.bed'
+sbatch mkDotPlot.py -x Oska$VERSION -y OSK42-1f  --dotplot-options='--bed1=~/OSKA_superscaffolding/oskaV2.bed'
+sbatch mkDotPlot.py -x Oska$VERSION -y AOM-5-1f  --dotplot-options='--bed1=~/OSKA_superscaffolding/oskaV2.bed'
 
 
