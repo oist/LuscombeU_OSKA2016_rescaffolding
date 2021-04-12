1eb2c51f28addb08daf55aaf33d3f7e9aac90876
Charles Plessy <charles.plessy@oist.jp>
2021-04-12 10:18:19 +0900

    V16

--- make_dotplot.sh
+++ make_dotplot.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
-module use /work/LuscombeU/modules/.modulefiles
-module load Oikopore
-module load LAST
-module load samtools
-module load blast
-module load emboss
+ml use /apps/unit/LuscombeU/.modulefiles/
+ml Oikopore
+ml LAST
+ml samtools
+ml ncbi-blast
+ml emboss
 
 mkLenghts () {
   infoseq @$1.usa -length -name -only -nohead -filter > $1.lengths.txt
@@ -24,20 +24,11 @@ do
   mkFASTA   $chr
 done
 
-VERSION=V15
+VERSION=V16
 
-mkdir /work/LuscombeU/Oikopleura/Genomes/Oska$VERSION
+srun -pshort seqret @oskaV2.usa Oska$VERSION.fa
 
-srun seqret @oskaV2.usa /work/LuscombeU/Oikopleura/Genomes/Oska$VERSION/Oska$VERSION.fa
-srun indexGenome.py -g Oska$VERSION
-cp ~/OSKA_superscaffolding/oskaV2.bed /work/LuscombeU/Oikopleura/Genomes/Oska$VERSION/
-
-alignTo () {
-  sbatch mkDotPlot.py -x Oska$VERSION -y $1 \
-    --dotplot-options="--bed1=/work/LuscombeU/Oikopleura/Genomes/Oska$VERSION/oskaV2.bed"
-}
-
-for genome in I69-4 Norway OSK42-1f AOM-5-1f O9-5f O3-3f B-2-1f
+for genome in OKI2018_I69_1.0 Norway OSK42-1f AOM-5-1f O9-5f O3-3f B-2-1f
 do
-  sbatch mkDotPlot.py -x Oska$VERSION -y $genome --dotplot-options="--bed1=/work/LuscombeU/Oikopleura/Genomes/Oska$VERSION/oskaV2.bed"
+  sbatch -pshort mkDotPlot.py -y Oska$VERSION -x $genome --dotplot-options="--bed2=oskaV2.bed"
 done
